7435fd589d776931bab4c0963cf630686c0baf89
Mateusz Szczech <matle0410@gmail.com>
2024-05-15 23:07:21 +0200

    Add Twitch OAuth authorization

---
+++ ChatReader/Dto/AccessTokenDto.cs
@@ -0,0 +1,8 @@
+struct AccessTokenDto
+{
+    public string access_token { get; set; }
+    public int expires_in { get; set; }
+    public string refresh_token { get; set; }
+    public List<string> scope { get; set; }
+    public string token_type { get; set; }
+}
--- ChatReader/Program.cs
+++ ChatReader/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Net.WebSockets;
+﻿using System.Net.Http.Headers;
+using System.Net.WebSockets;
+using System.Net;
+using System.Diagnostics;
+using System.Text.Json;
 using System.Text;
 using ChatReader;
 
@@ -6,37 +10,110 @@ var root = Directory.GetCurrentDirectory();
 var dotenv = Path.Combine(root, ".env");
 DotEnv.Load(dotenv);
 
-string? token = Environment.GetEnvironmentVariable("TOKEN");
-string? nick = Environment.GetEnvironmentVariable("NICK");
-string? channel = Environment.GetEnvironmentVariable("CHANNEL");
+string clientId = Environment.GetEnvironmentVariable("CLIENT_ID")!;
+string clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET")!;
 
-if (token == null || nick == null || channel == null)
+Console.WriteLine("What is your nick?");
+string? nick = Console.ReadLine();
+if (nick == null)
 {
-    Console.Error.WriteLine("Empty env variables");
+    Console.Error.WriteLine("Nick cannot be empty");
+    return;
+}
+
+string url = "https://id.twitch.tv/oauth2/";
+string responseType = "code";
+string redirect_uri = "http://localhost:3000/";
+string scope = "chat:read";
+string requestUrl = $"{url}authorize?response_type={responseType}&client_id={clientId}&redirect_uri={redirect_uri}&scope={scope}";
+
+Process.Start(new ProcessStartInfo(requestUrl) { UseShellExecute = true });
+
+HttpListener httpListener = new();
+httpListener.Prefixes.Add(redirect_uri);
+httpListener.Start();
+
+string? code;
+HttpListenerContext context;
+HttpListenerResponse response;
+do
+{
+    context = await httpListener.GetContextAsync();
+    var request = context.Request;
+    code = request.QueryString.Get("code");
+    response = context.Response;
+    if (code == null)
+    {
+        response.StatusCode = (int)HttpStatusCode.BadRequest;
+        response.Close();
+    }
+} while (code == null);
+
+string responseString = "Authentication completed successfully";
+byte[] resBuffer = Encoding.UTF8.GetBytes(responseString);
+response.ContentLength64 = resBuffer.Length;
+
+Stream output = response.OutputStream;
+output.Write(resBuffer, 0, resBuffer.Length);
+output.Close();
+
+httpListener.Stop();
+
+HttpClient httpClient = new()
+{
+    BaseAddress = new Uri(url),
+};
+httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+var data = new Dictionary<string, string>
+{
+    {"client_id", clientId},
+    {"client_secret", clientSecret},
+    {"code", code},
+    {"grant_type", "authorization_code"},
+    {"redirect_uri", redirect_uri}
+};
+FormUrlEncodedContent form = new(data);
+var res = await httpClient.PostAsync("token", form);
+string msg = await res.Content.ReadAsStringAsync();
+var twitchData = JsonSerializer.Deserialize<AccessTokenDto>(msg);
+
+string? token = twitchData.access_token;
+if (token == null)
+{
+    Console.Error.WriteLine("Invalid token");
+    return;
+}
+
+Console.WriteLine("What channel do you want to join?");
+string? channel = Console.ReadLine();
+if (channel == null)
+{
+    Console.Error.WriteLine("Channel cannot be empty");
     return;
 }
 
 Console.Title = "ChatReader Client";
 
-using (ClientWebSocket client = new())
+using (ClientWebSocket webSocket = new())
 {
     try
     {
-        await client.ConnectAsync(new Uri("wss://irc-ws.chat.twitch.tv:443"), CancellationToken.None);
+        await webSocket.ConnectAsync(new Uri("wss://irc-ws.chat.twitch.tv:443"), CancellationToken.None);
         var buffer = new byte[256];
         var sb = new StringBuilder();
 
         Console.WriteLine("Connection opened");
-        await client.SendAsync(Encoding.UTF8.GetBytes($"PASS oauth:{token}"), WebSocketMessageType.Text, true, CancellationToken.None);
-        await client.SendAsync(Encoding.UTF8.GetBytes($"NICK {nick}"), WebSocketMessageType.Text, true, CancellationToken.None);
-        await client.SendAsync(Encoding.UTF8.GetBytes($"JOIN #{channel}"), WebSocketMessageType.Text, true, CancellationToken.None);
+        await webSocket.SendAsync(Encoding.UTF8.GetBytes($"PASS oauth:{token}"), WebSocketMessageType.Text, true, CancellationToken.None);
+        await webSocket.SendAsync(Encoding.UTF8.GetBytes($"NICK {nick}"), WebSocketMessageType.Text, true, CancellationToken.None);
+        await webSocket.SendAsync(Encoding.UTF8.GetBytes($"JOIN #{channel}"), WebSocketMessageType.Text, true, CancellationToken.None);
 
-        while (client.State == WebSocketState.Open)
+        while (webSocket.State == WebSocketState.Open)
         {
             WebSocketReceiveResult? result = null;
             do
             {
-                result = await client.ReceiveAsync(buffer, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
                 sb.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
             } while (!result.EndOfMessage);
 
