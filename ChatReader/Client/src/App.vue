c2b75d560c92ef660606a459dda5232f12fb8b67
Mateusz Szczech <matle0410@gmail.com>
2024-06-17 22:07:21 +0200

    Add client auth

--- ChatReader/Client/src/App.vue
+++ ChatReader/Client/src/App.vue
@@ -1,11 +1,53 @@
-<script setup lang="ts">
+<script lang="ts">
+const nick = import.meta.env.VITE_NICK;
+const userId = import.meta.env.VITE_USER_ID;
+const userSecret = import.meta.env.VITE_USER_SECRET;
+
+export default {
+  methods: {
+    async auth() {
+      const url = "http://localhost:5293/auth/";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nick, userId, userSecret }),
+      });
+
+      if (response.ok) {
+        window.location.href = `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${userId}&redirect_uri=${url}&scope=chat:read`
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <main>
-    <p>Hello chat reader</p>
+    <h1 class="header">Authenticate with Twitch</h1>
+    <button class="button" @click="auth">
+      <span>Connect with Twitch</span>
+    </button>
   </main>
 </template>
 
 <style scoped>
+.header {
+  color: #fffefe;
+}
+
+.button {
+  background-color: #9046ff;
+  color: #fffefe;
+  padding: 6px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #772de9
+}
 </style>
--- ChatReader/Program.cs
+++ ChatReader/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:8080")
+            policy.WithOrigins("http://localhost:5173")
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials();
