5ee7c647420e1bc346d82a2dc04dcac42502d611
Mateusz Szczech <matle0410@gmail.com>
2024-06-29 20:46:31 +0200

    Initial v2 project

--- ChatReader/Program.cs
+++ ChatReader/Program.cs
@@ -1,43 +1,6 @@
-using ChatReader.Data;
-using ChatReader.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<UserData>();
-builder.Services.AddSingleton<IWebSocketClient, TwitchWebSocketClient>();
-builder.Services.AddSingleton<WebSocketService>();
-
-builder.Services.AddControllers();
-builder.Services.AddHttpClient();
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-});
-
 var app = builder.Build();
 
-app.UseRouting();
-app.UseCors();
-app.UseAuthorization();
-
-app.UseFileServer();
-
-var webSocketOptions = new WebSocketOptions
-{
-    KeepAliveInterval = TimeSpan.FromMinutes(2)
-};
-
-app.UseWebSockets(webSocketOptions);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- ChatReader/Services/IWebSocketClient.cs
+++
@@ -1,8 +0,0 @@
-namespace ChatReader.Services;
-
-public interface IWebSocketClient
-{
-    public void Start(string token, CancellationToken cancellationToken);
-    public Task SendMessage(string message, CancellationToken cancellationToken);
-    public string? DequeueParsedMessage();
-}
